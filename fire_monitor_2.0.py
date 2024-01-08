f8333b343af802584695aa26a083db0b040ff514
SarrangTech <113297733+SarrangTech@users.noreply.github.com>
2024-01-08 19:12:07 +0530

    Update fire_monitor_2.0.py

--- fire_monitor_2.0.py
+++ fire_monitor_2.0.py
@@ -136,8 +136,10 @@ def main():
         unsafe_allow_html=True
     )
 
-    uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
-    uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
+
+    
+    uploaded_file = st.file_uploader('Upload an image', key="file_uploader", type=['jpg', 'jpeg', 'png'])
+
 
     if uploaded_file is not None:
         # Create the 'uploads' directory if it doesn't exist
